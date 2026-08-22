11e5e16430fcffbaa0504c5df4d6633a36ea1547
Ishit06 <iamishit06@gmail.com>
2026-08-22 14:54:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (33.23%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -19,13 +19,15 @@ class Solution {
         Stack<TreeNode>stack=new Stack<>();
         TreeNode node=root;
         while(!stack.isEmpty() || node!=null){
-            while(node!=null){
+            if(node!=null){
 stack.push(node);
                 node=node.left;
             }
+            else{
             node=stack.pop();
             res.add(node.val);
             node=node.right;
+            }
         }
         return res;
 
