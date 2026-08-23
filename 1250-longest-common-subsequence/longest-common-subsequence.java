85e923d9ae1a0af6486e3e920b1bf2e9a43d2533
Ishit06 <iamishit06@gmail.com>
2026-08-23 17:57:41 +0530

    Time: 21 ms (54.71%) | Memory: 53.7 MB (81.74%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,29 +3,25 @@ class Solution {
         int m=text1.length();
         int n=text2.length();
         int dp[][]=new int[m+1][n+1];
-        for(int i=0;i<m+1;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return recur(text1,text2,m,n,dp);
-    }
-    public int recur(String text1,String text2,int i,int j,int dp[][]){
-        if(i==0 || j==0){
-            dp[i][j]=0;
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(text1.charAt(i-1)==text2.charAt(j-1)){
-            dp[i][j]= 1+recur(text1,text2,i-1,j-1,dp);
-            return dp[i][j];
+        dp[m][n]=0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                 if(text1.charAt(i-1)==text2.charAt(j-1)){
+            dp[i][j]= 1+ dp[i-1][j-1] ;//recur(text1,text2,i-1,j-1,dp);
+           // return dp[i][j];
         }
         else{
-           int case1=recur(text1,text2,i-1,j,dp);
-            int case2=recur(text1,text2,i,j-1,dp);
+           int case1= dp[i-1][j] ;//ecur(text1,text2,i-1,j,dp);
+            int case2=   dp[i][j-1];//recur(text1,text2,i,j-1,dp);
             dp[i][j]= Math.max(case1,case2);
-            return dp[i][j];
+            
         }
-        
+            }
+        }
+        return dp[m][n]; //recur(text1,text2,m,n,dp);
     }
+   
+      
+        
+    
 }
